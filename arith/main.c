546b41800e69036db287ac3e300bc45b54557d66
Pushpendra-Darky <pushpenra.assassin@gmail.com>
2022-05-23 22:46:30 +0530

    Arith_prog Demo

--- arith/main.c
+++ arith/main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("\nArith Demo\n\n");
+    int result=0;
+    printf("\nArith Demo\n");
+    result=add(35,34);
+    printf("\nADDITION RESULT IS: %d\n",result);
     return 0;
 }
