06589b2533aca6a90740e4699cb2b7a091bdef06
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2022-06-03 00:34:29 +0530

    Copy Utility Using System Call(Assignment1)

---
+++ Asg1_SysCall_Utility_to_Copy_Files/cps.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define MAX_SIZE 10240
+
+int main(int argv[], char *argc[])
+{
+    int fdr,fdw;
+    int size;
+    size_t rd,wr;
+    unsigned char buff[MAX_SIZE];
+
+    fdr=open(argc[1],O_RDONLY);
+    if(-1==fdr)
+    {
+        perror("\nError Opening Source File\n");
+        return -1;
+    }
+
+    fdw=open(argc[2], O_CREAT|O_RDWR,0777);
+    if(-1==fdr)
+    {
+        perror("Error Opening Destinaton File\n");
+        return -1;
+    }
+    size =atoi(argc[1]);
+    if (size > MAX_SIZE)
+    {
+        perror("File Size Exceeded\n");
+        return -1;
+    }
+    while(1)
+    {
+        rd=read(fdr, buff,MAX_SIZE);
+        
+        if (0 ==rd)
+        {
+            break;
+        }
+        wr=write(fdw,buff,rd); 
+
+        if (0 ==wr)
+        {
+            break;
+        }  
+    }
+    close(fdr);
+    close(fdw);
+    printf("Copied Data Into %s\n", argc[2]);
+    return 0;
+}
