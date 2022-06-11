4a5e304c419471fffbc261f8260507f5c5e2efc3
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2022-06-11 23:30:36 +0530

    Shared Memory Program

---
+++ Shared_memory/p2_read.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int *count = NULL;
+
+int main()
+{
+    int shm_fd;
+
+    shm_fd = shm_open("/shared_shm",O_CREAT | O_RDWR,S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);  // 0660
+
+    if(-1 == shm_fd)
+    {
+        perror("shm_open: Error In Opening\n");
+        return (EXIT_FAILURE);
+    }
+
+    ftruncate(shm_fd,sizeof(int));
+
+    count = (int *)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE, MAP_SHARED,shm_fd,0);
+
+    while(1)
+    {
+        printf("Data Recieved : By Shared Memory : %d\n",*count);
+    }
+    return 0;
+}
