f575ed0c96269ab11ee50c13e5f1c71d49d5de9d
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2022-06-13 01:32:58 +0530

    Socket Programming

---
+++ Socket_Program/client.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<linux/in.h>
+
+int cfd;
+
+struct sockaddr_in saddr;
+
+int len;
+
+unsigned char buff[1024];
+
+int main()
+{
+    cfd = socket(AF_INET,SOCK_STREAM,0);
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    saddr.sin_port = htons(5678);
+
+    connect(cfd,(struct sockaddr *)&saddr,sizeof(struct sockaddr_in));
+
+    read(cfd,buff,1024);
+    printf("Server Sent: %s\n",buff);
+
+    write(cfd,"Hello Server\n",13);
+
+    close(cfd);
+    
+    return 0;
+}
---
+++ Socket_Program/server.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<linux/in.h>
+
+int sfd;
+int cfd;
+
+struct sockaddr_in saddr;
+struct sockaddr_in caddr;
+
+int len;
+
+unsigned char buff[1024];
+
+int main()
+{
+    sfd = socket(AF_INET,SOCK_STREAM,0);
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    saddr.sin_port = htons(5678);
+
+    bind(sfd,(struct sockaddr *)&saddr,sizeof(struct sockaddr_in));
+    listen(sfd,10);
+
+    len = sizeof(struct sockaddr_in);
+
+    cfd = accept(sfd,(struct sockaddr *)&caddr,&len);
+    write(cfd,"Hello Client\n",13);
+
+    read(cfd,buff,1024);
+    printf("Client Sent: %s\n",buff);
+
+    close(sfd);
+    close(cfd);
+
+    return 0;
+}
